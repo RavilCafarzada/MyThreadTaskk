b2cbd1671c10f565e6ddbb46df79791146ca96f2
ravilj <ravilshJ@code.edu.az>
2024-01-12 06:40:54 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using ConsoleApp6;
+using Newtonsoft.Json;
+
+    string path = "https://jsonplaceholder.typicode.com/posts";
+    var response = await GetInfoAsync(path);
+
+    // Convert to objects
+    var myDatas = JsonConvert.DeserializeObject<MyData[]>(response);
+
+    // Specify the IDs
+    var specifiedIds = new List<int> { 1, 10, 100 };
+
+    // Filtered the specified IDs
+    var filteredObjects = myDatas.Where(obj => specifiedIds.Contains(obj.Id)).ToList();
+
+    string filePath = "mydata.json";
+
+    File.WriteAllText(filePath, JsonConvert.SerializeObject(filteredObjects, Formatting.Indented));
+
+    Console.WriteLine($"Filtered objects with specified IDs have been written to {filePath}");
+
+
+static async Task<string> GetInfoAsync(string path)
+{
+    using (HttpClient client = new HttpClient())
+    {
+        string result = await client.GetStringAsync(path);
+        return result;
+    }
+}
